606c3f1374f14bf3c2d48b08398c691a6e0e0295
icg-lyoo <lyoo@icginfo.com>
2013-07-26 11:10:54 -0700

    deleting all files

--- include/display_control/sharpCommands.php
+++
@@ -1,9 +0,0 @@
-<?php
-//array of commands
-$commands = array('set', 'get');
-$commands['set'] = array('on', 'off');
-
-// "data" parameters of each command
-$commands['set']['on'] = array('POWR', '1');
-$commands['set']['off'] = array('POWR', '0');
-?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-     ini_set('display_errors', true);
-     include_once('./include/MenuBoard.php');
-     
-     MenuBoard::$clientclass = 'MCDV1';
-     
-     $mb = new MenuBoard();
-     $mb->generateContent();
-?>
